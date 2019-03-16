8b294743727ef54dc4d300082674668755c095f1
Rebecca Strauss <rstrauss127@gmail.com>
2019-03-16 20:06:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 function lowerCaseDrivers(drivers) {
   return drivers.map(driver => driver.toLowerCase());
 }
+
+function nameToAttributes(drivers) {
+  return drivers.map(drivers, function (d) {
+    Object.assign({}, firstName: drivers[0], lastName: drivers[1]);
+  })
+}
