21b0d8cc01c0c13a9a1bdc52437d1e6d28940329
Rebecca Strauss <rstrauss127@gmail.com>
2019-03-16 20:31:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution in this file.
+function lowerCaseDrivers(drivers) {
+  return drivers.map(driver => driver.toLowerCase());
+}
+
+function nameToAttributes(drivers) {
+
+  return drivers.map(function (person) {
+    fullname = person.split(" ")
+    first = fullname[0];
+    last = fullname[1];
+    return {firstName: first, lastName: last};
+  }
+
+}
